e226df4f10217d0844c77b110d1652cc61a9b61c
Schokiiiiiiii <fabien@leger-dev.swiss>
2026-02-18 11:58:27 +0100

    Finished exo1

--- c/exo1.c
+++ c/exo1.c
@@ -11,12 +11,29 @@
 #include "tests.h"
 
 char *allocate_string(const char *string, int length) {
-    // TODO
-    return NULL;
+    
+    if (string == NULL)
+        return NULL;
+
+    char *alloc = malloc(sizeof(char) * length + 1);
+    if (alloc == NULL)
+        return NULL;
+    
+    for (int i = 0 ; i < length ; ++i)
+        *(alloc + i) = *(string + i);
+
+    *(alloc + length) = '\0';
+
+    return alloc;
 }
 
 int main(int argc, char **argv) {
     printf("Welcome to ASM\n");
+    const char *string = "Bonjour de main!";
+
+    char *alloc = allocate_string(string, strlen(string));
+
+    printf("Received allocated message : %s\n", alloc);
 
     tests_exo1();
     return 0;
